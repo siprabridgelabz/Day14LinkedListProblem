68f2651b39dcfe15e7b894222e857ef8aedb2620
sipra <sipramishra085@gmail.com>
2023-04-03 00:28:22 +0530

    Inserted of Data ..Added UC4

--- Program.cs
+++ Program.cs
@@ -6,11 +6,12 @@
         {
             Console.WriteLine("Welcome to Linked list operation");
             LinkedList list = new LinkedList();
-            list.Add(30);
             list.Add(56);
+            list.Add(30);
             list.Add(70);
 
             list.Display();
+            Console.WriteLine("search element" +list.Search(30));
         }
     }
 }
