29e1ddfc709e3ced31cf1c639a9a8b8e35c5fe58
sipra <sipramishra085@gmail.com>
2023-04-02 22:52:42 +0530

    Added UC1

---
+++ LinkedList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day14LinkedList
+{
+    internal class LinkedList
+    {
+        
+        
+        internal Node head;
+
+        internal void Add(int data)
+       
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while(temp.next !=null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+           
+            Console.WriteLine("{0} Inserted into Linked List", node.data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+
+
+
+}
