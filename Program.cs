cd30fca9d7774f3c5ea50b4cb9f3227e7ad80611
sipra <sipramishra085@gmail.com>
2023-04-02 22:47:03 +0530

    Added program file

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Day14LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked list operation");
+            LinkedList list = new LinkedList();
+            list.Add(30);
+            list.Add(56);
+            list.Add(70);
+
+            list.Display();
+        }
+    }
+}
